7a50da447e4f5f845cd2cf1f3a8060b018d554db
greeneley <anhchangtoanhoc97@gmail.com>
2025-10-12 21:57:02 +0700

    asaas

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,6 @@ const startServer = async () => {
 startServer();
 const PORT = process.env.PORT || 3000;
 
-
-console.log("TEST");
 app.listen(3000, () => {
     console.log(`server running in ${PORT} port`)
 })
